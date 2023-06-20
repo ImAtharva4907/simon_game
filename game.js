05b1a316f7a56a6c33637ea96f088711ff39ad37
Atharva chauhan <104714726+ImAtharva4907@users.noreply.github.com>
2023-06-20 06:35:20 +0000

    functions created

--- game.js
+++ game.js
@@ -1,10 +1,29 @@
 var buttonColours = ["red","blue","green","yellow"];
-
+var userClickedPattern = [];
 var gamePattern = [];
 
+$(".btn").click(function(){
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+
+})
 function nextSequence()
 {
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+}
+function playSound(name)
+{
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
+function animatePress(currentColour)
+{
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){
+        
+    })
 }
